f0c1e91d882c83fd5a5e3bb07bf9e0b581c46ee0
Jstmaul <maunadhif1@gmail.com>
2024-10-04 21:20:59 +0700

    yes

--- lexer.c
+++ lexer.c
@@ -1,6 +1,7 @@
 #include "lexer.h"
 #include "type.h"
 #include <ctype.h>
+#include <stdio.h>
 
 data_struct *maulLexer(FILE *fp, int *token_count) {
   int ch;
@@ -11,33 +12,41 @@ data_struct *maulLexer(FILE *fp, int *token_count) {
 
   data_struct *data =
       (data_struct *)malloc(data_capacity * sizeof(data_struct));
+  if (data == NULL) {
+    printf("FAILED ALLOC DATA\n");
+    exit(EXIT_FAILURE);
+  }
 
   while ((ch = (int)fgetc(fp)) != EOF) {
     printf("lexer : %c\n", ch);
 
-    if (i >= (data_capacity - 1)) {
+    if (i >= (data_capacity)) {
       data_capacity++;
       data = realloc(data, data_capacity * sizeof(data_struct));
+      if (data == NULL) {
+        printf("FAILED ALLOC DATA\n");
+        exit(EXIT_FAILURE);
+      }
     }
     if (isalpha(ch)) {
       data[i].character = ch;
       data[i].character_type = "name";
       printf("lexing succes\n");
-    }
-    if (isdigit(ch)) {
+    } else if (isdigit(ch)) {
       data[i].character = ch;
       data[i].character_type = "digit";
       printf("lexing succes\n");
-    }
-    if (ch == '\n') {
+    } else if (ch == '\n') {
       data[i].character = ch;
       data[i].character_type = "new_line";
       printf("lexing succes\n");
-    }
-    if (ch == ' ') {
+    } else if (ch == ' ') {
       data[i].character = ch;
       data[i].character_type = "space";
       printf("lexing succes\n");
+    } else {
+      printf("unworked character :  %d", ch);
+      continue;
     }
     i++;
   }
